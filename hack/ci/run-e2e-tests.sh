0e244910bb1de50cbf73056fdc9b69e697f411e3
Christoph Mewes <christoph@kubermatic.com>
2025-02-07 21:40:15 +0100

    start kcp natively without kind, use envtest
    On-behalf-of: @SAP christoph.mewes@sap.com

--- hack/ci/run-e2e-tests.sh
+++ hack/ci/run-e2e-tests.sh
@@ -15,65 +15,45 @@
 # limitations under the License.
 
 set -euo pipefail
+source hack/lib.sh
 
-# build the image(s)
-export IMAGE_TAG=local
+# build the agent, we will start it many times during the tests
+echodate "Building the api-syncagent…"
+make build
 
-echo "Building container images…"
-# ARCHITECTURES=amd64 DRY_RUN=yes ./hack/ci/build-image.sh
+# get kube envtest binaries
+echodate "Setting up Kube binaries…"
+make _tools/setup-envtest
+export KUBEBUILDER_ASSETS="$(_tools/setup-envtest use 1.31.0 --bin-dir _tools -p path)"
+KUBEBUILDER_ASSETS="$(realpath "$KUBEBUILDER_ASSETS")"
 
-# start docker so we can run kind
-# start-docker.sh
+# start a shared kcp process
+make _tools/kcp
 
-# create a local kind cluster
-KIND_CLUSTER_NAME=e2e
+KCP_ROOT_DIRECTORY=.kcp.e2e
+KCP_LOGFILE=kcp.e2e.log
 
-# echo "Preloading the kindest/node image…"
-# docker load --input /kindest.tar
+echodate "Starting kcp…"
+rm -rf "$KCP_ROOT_DIRECTORY" "$KCP_LOGFILE"
+_tools/kcp start \
+  --root-directory "$KCP_ROOT_DIRECTORY" 1>"$KCP_LOGFILE" 2>&1 &
 
-export KUBECONFIG=$(mktemp)
-export KUBECONFIG=e2e.kubeconfig
-echo "Creating kind cluster $KIND_CLUSTER_NAME…"
-kind create cluster --name "$KIND_CLUSTER_NAME"
-chmod 600 "$KUBECONFIG"
+stop_kcp() {
+  echodate "Stopping kcp processes…"
+  pkill -e kcp
+}
+append_trap stop_kcp EXIT
 
-# load the agent image into the kind cluster
-image="ghcr.io/kcp-dev/api-syncagent:$IMAGE_TAG"
-archive=agent.tar
+# Wait for kcp to be ready; this env name is also hardcoded in the Go tests.
+export KCP_KUBECONFIG="$KCP_ROOT_DIRECTORY/admin.kubeconfig"
 
-echo "Loading api-syncagent image into kind…"
-buildah manifest push --all "$image" "oci-archive:$archive:$image"
-kind load image-archive "$archive" --name "$KIND_CLUSTER_NAME"
-
-# deploy cert-manager
-echo "Installing cert-manager…"
-
-helm repo add jetstack https://charts.jetstack.io
-helm repo update
-
-kubectl apply --filename https://github.com/cert-manager/cert-manager/releases/download/v1.17.0/cert-manager.crds.yaml
-helm install \
-  --wait \
-  --namespace cert-manager \
-  --create-namespace \
-  --version v1.17.0 \
-  cert-manager jetstack/cert-manager
-
-# deploy a kcp which will live for the entire runtime of the e2e tests and be shared among all subtests
-echo "Installing kcp into kind…"
-
-helm repo add kcp https://kcp-dev.github.io/helm-charts
-helm repo update
-
-helm install \
-  --wait \
-  --namespace kcp \
-  --create-namespace \
-  --values hack/ci/testdata/kcp-kind-values.yaml \
-  kcp-e2e kcp/kcp
+if ! retry_linear 3 20 kubectl --kubeconfig "$KCP_KUBECONFIG" get logicalcluster; then
+  echodate "kcp never became ready."
+  exit 1
+fi
 
 # time to run the tests
-echo "Running e2e tests…"
+echodate "Running e2e tests…"
 (set -x; go test -tags e2e -timeout 2h -v ./test/e2e/...)
 
-echo "Done. :-)"
+echodate "Done. :-)"
